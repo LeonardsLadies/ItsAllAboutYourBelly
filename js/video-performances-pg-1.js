a87e3db9d58464bc360bfeb80de9f2eeb7e236eb
LeonardsLadies <LeonardsLadies@gmail.com>
2026-06-23 07:45:06 -0400

    Update video-performances-pg-1.js
    Delete commented out URL list

--- js/video-performances-pg-1.js
+++ js/video-performances-pg-1.js
@@ -20,50 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
         { url: "https://youtu.be/2ESs9pAnKHM", thumbnail: "https://img.youtube.com/vi/2ESs9pAnKHM/hqdefault.jpg", title: "Milk & Honey<br>Spring Hafla" }
     ];
 
-    /* Array of video URLs
-    const videos = [
-        {
-            url: "https://youtu.be/NoeuI8PJ-Nc",
-            thumbnail: "https://img.youtube.com/vi/NoeuI8PJ-Nc/hqdefault.jpg",
-            title: "Iko Iko<br>Under the Sea Hafla"
-        },
-        {
-            url: "https://youtu.be/7oCSdcDf3mo",
-            thumbnail: "https://img.youtube.com/vi/7oCSdcDf3mo/hqdefault.jpg",
-            title: "Viking<br>Sping Hafla"
-        },
-        {
-            url: "https://youtu.be/5HLnoWRLMMg",
-            thumbnail: "https://img.youtube.com/vi/5HLnoWRLMMg/hqdefault.jpg",
-            title: "I'm on Fire!!<br>Spring Hafla"
-        },
-        {
-            url: "https://youtu.be/4b8osGhImyo",
-            thumbnail: "https://img.youtube.com/vi/4b8osGhImyo/hqdefault.jpg",
-            title: "Drums<br>Ren Faire"
-        },
-        {
-            url: "https://youtu.be/-tEwNn0-pwA",
-            thumbnail: "https://img.youtube.com/vi/-tEwNn0-pwA/hqdefault.jpg",
-            title: "Country Fusion<br>Greek Hafla"
-        },
-        {
-            url: "https://youtu.be/XktITc3CMVk",
-            thumbnail: "https://img.youtube.com/vi/XktITc3CMVk/hqdefault.jpg",
-            title: "Thriller<br>Oldsmar Talent Show"
-        },
-        {
-            url: "https://youtu.be/j3kOrEKzwrA",
-            thumbnail: "https://img.youtube.com/vi/j3kOrEKzwrA/hqdefault.jpg",
-            title: "Folkloric<br>Ren Faire"
-        },
-        {
-            url: "https://youtu.be/AQTzPlDu3Uk",
-            thumbnail: "https://img.youtube.com/vi/AQTzPlDu3Uk/hqdefault.jpg",
-            title: "Samba<br>Ren Faire"
-        }
-    ]; */
-
     videos.forEach(video => {
         const colDiv = document.createElement('div');
         colDiv.className = 'col-lg-3 col-md-4 col-sm-6 mb-4';
